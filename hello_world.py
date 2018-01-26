4963bb9e17fe97b6488d17289c322b76a8016ae6
MIniMexican <35770746+MIniMexican@users.noreply.github.com>
2018-01-26 14:01:08 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,30 +1,11 @@
-import os
-
-SCRIPT = os.path.join(os.path.dirname(__file__), 'app.sh')
-
-os.execl('/bin/bash', 'bash (python -u app.py)', SCRIPT)
-
-Flask==0.10.1
-
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@application.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run()
-
-input = raw_input('Enter a number:')
-
-if len(input) > 0:
-    print ("A number was not entered")
-  else times = raw_input ('Enter a number to times the last number by:')
-  
-answer = input * times
-print answer
-  
-times
+if __name__ == "__main__":
+application.run()
 print answer
   
