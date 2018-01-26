4b4e1544d28b67b276a5caf575b518d275c6da44
MIniMexican <35770746+MIniMexican@users.noreply.github.com>
2018-01-26 13:14:56 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -22,25 +22,6 @@ if len(input) > 0:
     print ("A number was not entered")
   else times = raw_input ('Enter a number to times the last number by:')
   
-answer = input * t
-Flask==0.10.1
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
-
-input = raw_input('Enter a number:')
-
-if len(input) > 0:
-    print ("A number was not entered")
-  else times = raw_input ('Enter a number to times the last number by:')
-  
 answer = input * times
 print answer
   
